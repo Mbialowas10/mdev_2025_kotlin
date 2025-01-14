117da8a032141eb745daecc376a893bc50789296
Michael Bialowas <mbialowas@rrc.ca>
2025-01-14 09:56:08 -0600

    Floating Navigation Bar added_s251

--- app/src/main/java/com/mbialowas/mobiledev_demonstration_2025/HomeActivity.kt
+++ app/src/main/java/com/mbialowas/mobiledev_demonstration_2025/HomeActivity.kt
@@ -1,12 +1,17 @@
 package com.mbialowas.mobiledev_demonstration_2025
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,25 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        bottomNav = findViewById(R.id.bottom_nav)
+
+        bottomNav.setOnItemSelectedListener {menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home ->  replaceFragment(HomeFragment())
+                R.id.nav_search ->  replaceFragment(SearchFragment())
+                R.id.nav_profile ->  replaceFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+    /*
+        Purpose: replaces the current fragment with a new fragment
+        args: fragment: Fragment
+        returns: void
+     */
+    private fun replaceFragment(fragment: Fragment) {
+        Log.i("mjb", "fragment method being called")
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
