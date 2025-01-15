25978152d4670636aa177d9ea02e23a68972c059
Michael Bialowas <mbialowas@rrc.ca>
2025-01-15 09:55:16 -0600

    Fragment_Demo_s249

---
+++ app/src/main/java/com/mbialowas/mobiledev_demonstration_2025/HomeActivity.kt
@@ -0,0 +1,43 @@
+package com.mbialowas.mobiledev_demonstration_2025
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var bottomNavBar: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        bottomNavBar = findViewById(R.id.bottomNavBar)
+
+        bottomNavBar.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_search -> replaceFragment(SearchFragment())
+                R.id.nav_profiles -> replaceFragment(ProfileFragment())
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        Log.i("mjb", "fragment method being called.")
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
